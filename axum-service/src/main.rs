a856534e76664c56b91120eb800c60a2b0dedef4
Dulyakit <dulyakit@thinknet.co.th>
2024-11-20 11:06:46 +0700

    update repo

--- axum-service/src/main.rs
+++ axum-service/src/main.rs
@@ -14,7 +14,6 @@ async fn main() {
     let app = Router::new()
         .route("/", get(hello_world));
 
-    // เริ่ม server ที่ port 3000
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     println!("Server running on http://127.0.0.1:3000");
     
